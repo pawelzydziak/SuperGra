20d1607728c2a9cd2aa502eaa6441611374c3562
Paweł Żydziak <pawel.zydziak@relayr.io>
2020-01-29 11:45:36 +0100

    fixes

--- src/Objects/Coin.gd
+++ src/Objects/Coin.gd
@@ -5,4 +5,5 @@ onready var animation_player: AnimationPlayer = $AnimationPlayer
 func _on_Coin_body_entered(body: PhysicsBody2D) -> void:
 	animation_player.play("fade")
 	PlayerVars.playerPoints += 100
+	queue_free()
 	print(PlayerVars.playerPoints)
